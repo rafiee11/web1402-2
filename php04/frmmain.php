746efbd840ffdd341fccc78de317ce0e2e87afa1
rafiee11 <159271547+rafiee11@users.noreply.github.com>
2024-10-22 10:26:57 +0330

    projetc1

--- php04/frmmain.php
+++ php04/frmmain.php
@@ -1,4 +1,5 @@
-<!-- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,15 +9,37 @@
 <body>
     <style>
         div{
-            width: 100px;
-        }
-    </style> -->
-    <form action="index.php" method="get">
+			width: 100px;
+			height: 100px;
+			margin:10px;
+			background-color:green;
+		}
+    </style>
+
+<?php
+$l=1;
+if (empty($_GET['lName'])) {
+?>
+<form method="get">
         <label >First Name</label><input type="text" name="fName">
         <label >Lirst Name</label><input type="text" name="lName">
         <label >Number</label><input type="number" name="number">
         <label >Color</label><input type="color" name="color">
         <input type="submit">
     </form>
+    <?php
+}
+else{ 
+    $fname=$_GET["fName"];
+	$lname=$_GET['lName'];
+	$number=$_GET['number'];
+	$color=$_GET['color'];
+    echo "<h2>$fname $lname</h2>";
+	for ($i=0; $i < $number; $i++) { 
+		echo "<div></div>";
+	}
+	
+}
+?>
 </body>
 </html>
